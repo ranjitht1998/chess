993339c8b168f725bb30cc6c32760eb4921e7464
ranjitht1998 <46715861+ranjitht1998@users.noreply.github.com>
2020-04-26 21:44:30 +0530

    Added function

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-let input = "N3";
+let input = "N4";
 
 let input1 = input.split("");
 let direction = input[0];
@@ -13,24 +13,32 @@ switch (direction) {
     queen.y += movement;
     if (queen.y > 7) {
       console.log("Out of boundary");
+    } else {
+      board(queen.x, queen.y);
     }
     break;
   case "S":
     y -= movement;
     if (y < 0) {
       console.log("Out of boundary");
+    } else {
+      board(queen.x, queen.y);
     }
     break;
   case "E":
     x.queen += movement;
     if (x.queen > 7) {
       console.log("Out of boundary");
+    } else {
+      board(queen.x, queen.y);
     }
     break;
   case "W":
     x.queen -= movement;
     if (x.queen < 0) {
       console.log("out of boundary");
+    } else {
+      board(queen.x, queen.y);
     }
     break;
   case "NE":
@@ -38,6 +46,8 @@ switch (direction) {
     y.queen += movement;
     if (x.queen > 7 || y.queen > 7) {
       console.log("out of boundary");
+    } else {
+      board(queen.x, queen.y);
     }
     break;
   case "SE":
@@ -45,6 +55,8 @@ switch (direction) {
     y.queen -= movement;
     if (x.queen > 7 || y.queen < 0) {
       console.log("out of boundary");
+    } else {
+      board(queen.x, queen.y);
     }
     break;
   case "SW":
@@ -52,6 +64,8 @@ switch (direction) {
     y.queen -= movement;
     if (x < 0 || y < 0) {
       console.log("out of boundary");
+    } else {
+      board(queen.x, queen.y);
     }
     break;
   case "NW":
@@ -59,26 +73,30 @@ switch (direction) {
     y.queen += movement;
     if (x < 0 || y > 7) {
       console.log("out of boundary");
+    } else {
+      board(queen.x, queen.y);
     }
     break;
 }
 
-let board = [
-  ["a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8"],
+function board(a, b) {
+  arr = [
+    ["a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8"],
 
-  ["a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7"],
+    ["a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7"],
 
-  ["a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6"],
+    ["a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6"],
 
-  ["a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5"],
+    ["a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5"],
 
-  ["a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4"],
+    ["a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4"],
 
-  ["a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3"],
+    ["a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3"],
 
-  ["a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2"],
+    ["a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2"],
 
-  ["a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"],
-];
+    ["a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"],
+  ];
 
-console.log("Queen's current Position ->" + board[queen.x][queen.y]);
+  console.log("Queen's current Position ->" + arr[a][b]);
+}
